ca366dca8dbb69e0361e47af36d865bc2ccc04db
IO-oINik <nik.mokshin.2004@mail.ru>
2024-04-07 17:26:28 +0300

    init commit

---
+++ GeneratorUrl/src/main/java/ru/edu/generatorurl/GeneratorUrlApplication.java
@@ -0,0 +1,13 @@
+package ru.edu.generatorurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorUrlApplication.class, args);
+    }
+
+}
