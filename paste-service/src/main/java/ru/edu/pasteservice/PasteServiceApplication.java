a938089000a89844ab6fc8525dfbfd8e74c07ec1
IO-oINik <nik.mokshin.2004@mail.ru>
2024-04-07 21:10:43 +0300

    create paste-service

---
+++ paste-service/src/main/java/ru/edu/pasteservice/PasteServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.edu.pasteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteServiceApplication.class, args);
+    }
+
+}
